8d17fd982235674a03092384f05ac9baaba189fa
aswin.a <aswin.a@olacabs.com>
2018-02-18 17:09:26 +0530

    remote exec file, first commit

---
+++ remote-executor.sh
@@ -0,0 +1,11 @@
+#$/bin/bash
+
+# Author : aswin.a
+#
+#
+# The script executes command where jenkins is executing this job.
+# We are also testing the github integration and automatic job trigger for commits
+
+echo "Hello world"
+hostname
+date
